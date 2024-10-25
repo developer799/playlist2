9e97965ca1ad44804e19bfb5d70719cb1ae83a36
developer799 <goun0203@naver.com>
2024-10-25 23:44:34 +0900

    app.py

--- app.py
+++ app.py
@@ -206,4 +206,4 @@ def serve_audio(filename):
     return send_from_directory(AUDIO_FOLDER, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
